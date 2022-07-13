cdfad9a3f743e28c2d41e3ba9aecc75a5e4b37f8
Matteo SCHIAVONI <matteo.schiavoni.consultant@quixa.it>
2022-07-13 17:51:35 +0200

    - added more tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -58,5 +58,80 @@ describe('AppComponent', () => {
 		it('should show the text \'Decrease\' into the right button', () => {
 			expect(decreaseBtn?.textContent).withContext('The text is not the expected one').toContain('Decrease');
 		});
+
+		describe('when the user clicks on the \'decreaseBtn\' element', () => {
+			afterEach(() => {
+				component.count = 0;
+			});
+
+      it('should call the expected action from the template', async () => {
+        spyOn(component, 'decreaseCount')
+          .and.callThrough();
+        
+        decreaseBtn.dispatchEvent(new Event('click'));
+				fixture.detectChanges();
+        await fixture.whenStable();
+				textDiv = fixture.debugElement.query(By.css('div#mainTxt')).nativeElement;
+
+				expect(component.decreaseCount).withContext('Method not called as expected').toHaveBeenCalled();
+				expect(component.count).withContext('Value not equal 0 as expected').toBe(0);
+				expect(textDiv).withContext('Text not rendered as expected').not.toBeNull();
+				expect(textDiv?.textContent).withContext('Text not equal to the expected one').toContain('Number of clicks: 0');
+      });
+    });
+
+		describe('when the user clicks on the \'increaseBtn\' element', () => {
+			afterEach(() => {
+				component.count = 0;
+			});
+
+			it('should call the expected action from the template', async () => {
+        spyOn(component, 'increaseCount')
+          .and.callThrough();
+        
+        increaseBtn.dispatchEvent(new Event('click'));
+				fixture.detectChanges();
+        await fixture.whenStable();
+				textDiv = fixture.debugElement.query(By.css('div#mainTxt')).nativeElement;
+
+				expect(component.increaseCount).withContext('Method not called as expected').toHaveBeenCalled();
+				expect(component.count).withContext('Value not equal 1 as expected').toBe(1);
+				expect(textDiv).withContext('Text not rendered as expected').not.toBeNull();
+				expect(textDiv?.textContent).withContext('Text not equal to the expected one').toContain('Number of clicks: 1');
+      });
+		});
+
+		describe('when the user clicks on the buttons several times', () => {
+			afterEach(() => {
+				component.count = 0;
+			});
+
+			it('should render the number of clicks based on the count variable', async () => {
+				let increaseAttempt: number = 4;
+				let decreaseAttempts: number = 3;
+
+				for (let i = 1; i <= increaseAttempt; i++) {
+					increaseBtn.dispatchEvent(new Event('click'));
+					fixture.detectChanges();
+					await fixture.whenStable();
+					textDiv = fixture.debugElement.query(By.css('div#mainTxt')).nativeElement;
+	
+					expect(component.count).withContext(`Value not equal ${i} as expected`).toBe(i);
+					expect(textDiv).withContext('Text not rendered as expected').not.toBeNull();
+					expect(textDiv?.textContent).withContext('Text not equal to the expected one').toContain(`Number of clicks: ${i}`);
+				}
+				
+				for (let i = 1; i < decreaseAttempts; i++) {
+					decreaseBtn.dispatchEvent(new Event('click'));
+					fixture.detectChanges();
+					await fixture.whenStable();
+					textDiv = fixture.debugElement.query(By.css('div#mainTxt')).nativeElement;
+
+					expect(component.count).withContext(`Value not equal ${increaseAttempt-i} as expected`).toBe(increaseAttempt-i);
+					expect(textDiv).withContext('Text not rendered as expected').not.toBeNull();
+					expect(textDiv?.textContent).withContext('Text not equal to the expected one').toContain(`Number of clicks: ${increaseAttempt-i}`);
+				}
+			});
+		});
 	});
 });
